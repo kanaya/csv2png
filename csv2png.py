2eba6f8eab83f04783556bf5ed28da03a4d79bb4
Ichi Kanaya <kanaya@pineapple.cc>
2019-07-28 19:57:13 +0900

    Initial commit.

---
+++ csv2png.py
@@ -0,0 +1,31 @@
+from PIL import Image
+import csv
+import sys
+
+input_fn = sys.argv[1]
+output_fn = sys.argv[2]
+
+csv_file = open(input_fn, "r")
+csv_image = csv.reader(csv_file, delimiter=',')
+image_width = 0
+image_height = 0
+raster = []
+for row in csv_image:
+	w = len(row)
+	raster.append(row)
+	image_height += 1
+	image_width = max(image_width, w)
+image = Image.new('RGB', (image_width, image_height))
+y = 0
+for r in raster:
+	x = 0
+	for p in r:
+		if p.strip() == '0':
+			c = 0
+		else:
+			c = 255
+		image.putpixel((x, y), (c, c, c))
+		x += 1
+	y += 1
+image.save(output_fn)
+
